96b8d93d05024e7b3e35549df68fc0eec5f7b298
Kenchie Villajuan <kenchievillajuan@gmail.com>
2023-01-25 12:42:46 +0800

    fix prod api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
     name: "production",
     theme: "light",
     production: true,
-    api: "http://localhost:3000/api",
+    api: "https://hello-client-server.onrender.com/api",
     imageKit: {
         publicKey: "public_xxZjiqGKP247HsziX7mB2W2BA6s=",
         endpoint: "https://ik.imagekit.io/zghagvfvqzy/"
